e7c603e2d26eac6000e5972613be0ef978695f51
Matheu <contato@matheusborba.com.br>
2026-04-26 23:38:48 -0300

    Ajustes - fases empreendimentos

---
+++ database/migrations/2026_04_26_250000_normalize_empreendimento_phase_status.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Normaliza valores legados da coluna `status` em empreendimentos pros
+ * 5 slugs canônicos da timeline de fases (E3):
+ *   - breve_lancamento
+ *   - lancamento
+ *   - em_obras
+ *   - obras_avancadas
+ *   - pronto_para_morar
+ */
+return new class extends Migration {
+    public function up(): void
+    {
+        $map = [
+            'breve lançamento'    => 'breve_lancamento',
+            'breve lancamento'    => 'breve_lancamento',
+            'pré-lançamento'      => 'breve_lancamento',
+            'pre-lancamento'      => 'breve_lancamento',
+            'lançamento'          => 'lancamento',
+            'em obras'            => 'em_obras',
+            'obras'               => 'em_obras',
+            'obras avançadas'     => 'obras_avancadas',
+            'obras avancadas'     => 'obras_avancadas',
+            'pronto'              => 'pronto_para_morar',
+            'pronto morar'        => 'pronto_para_morar',
+            'pronto_morar'        => 'pronto_para_morar',
+            'pronto para morar'   => 'pronto_para_morar',
+            'pronto pra morar'    => 'pronto_para_morar',
+            'pronto_pra_morar'    => 'pronto_para_morar',
+            'entregue'            => 'pronto_para_morar',
+        ];
+
+
+        foreach ($map as $from => $to) {
+            DB::table('empreendimentos')
+                ->whereRaw('LOWER(TRIM(status)) = ?', [$from])
+                ->update(['status' => $to]);
+        }
+    }
+
+    public function down(): void
+    {
+
+    }
+};
